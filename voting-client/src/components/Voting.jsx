4d2cef434900dd95076f79290627284959a02375
theo tran <theotran@rocketmail.com>
2017-01-10 18:23:14 -1000

    INIT

---
+++ voting-client/src/components/Voting.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default React.createClass({
+  getPair: function (){
+    return this.props.pair || [];
+  },
+  isDisabled: function (){
+    return !!this.props.hasVoted;
+  },
+  render: function () {
+    return (
+      <div className="voting">
+        {this.getPair().map(entry => 
+          <button key={entry}
+                  disabled={this.isDisabled()}
+                  onClick={() => this.props.vote(entry)}>
+            <h1>{entry}</h1>
+          </button>
+        )}
+      </div>
+    )
+  }
+});
